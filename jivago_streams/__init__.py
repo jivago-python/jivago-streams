d378a73b5eb1dbe90da0abba5a6f42b60c614412
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-03-06 14:17:48 -0500

    nullables

--- jivago_streams/__init__.py
+++ jivago_streams/__init__.py
@@ -1,11 +1,105 @@
 import inspect
 import itertools
-from typing import Iterable, Callable, Iterator, Optional, Tuple, Any, TypeVar, Generic, Set, List
+from typing import Iterable, Iterator, Tuple, Any, TypeVar, Set, List, Generic, Optional, Union, Callable
 
 __version__ = '@@VERSION@@'
 
 T = TypeVar('T')
 S = TypeVar('S')
+
+
+class Nullable(Generic[T]):
+    """Nullable class which wraps Optional types.
+
+    Args:
+        nullable (Optional[object)) : Item which can be None.
+    """
+
+    def __init__(self, nullable: Optional[T]):
+        self._item: Optional[T] = nullable
+
+    def isPresent(self) -> bool:
+        """Returns True if item is not None."""
+        return self._item is not None
+
+    def get(self) -> Optional[T]:
+        """Gets the item if present.
+
+        Raises:
+            EmptyNullableException: Attempting to get a missing item.
+            """
+        if self.isPresent():
+            return self._item
+        raise EmptyNullableException()
+
+    def orElse(self, default_value: T) -> T:
+        """Returns the item if present, else return the supplied default value.
+        Args:
+            default_value: Value to return instead of a None value.
+            """
+        return self._item if self.isPresent() else default_value
+
+    def orElseThrow(self, exception: Union[Exception, Callable[[], Exception]]) -> T:
+        """Returns if present, raises exception if missing.
+        Args:
+            exception: Either an exception, or a callable which returns an exception.
+        """
+        if self.isPresent():
+            return self._item
+        if isinstance(exception, Exception):
+            raise exception
+        raise exception()
+
+    def orElseFetch(self, supplier: Callable[[], T]) -> T:
+        """Returns if present, invoke callable if missing.
+        Args:
+            supplier (Callable): Supplied return value will be return in place of a None value. Should not require parameters.
+        """
+        if self.isPresent():
+            return self._item
+        return supplier()
+
+    def ifPresent(self, consumer: Callable[[T], None]) -> None:
+        """Invoke function if value is present; otherwise does nothing.
+        Args:
+            consumer (Callable): Function to be invoked with a non-nil parameter.
+        """
+        if self.isPresent():
+            consumer(self._item)
+
+    def filter(self, predicate: Callable[[T], bool]) -> "Nullable[T]":
+        """Filters item given a criterion.
+        Args:
+            predicate (Callable): Invoked with a non-nil parameter. Should return a boolean.
+
+        """
+        if self.isPresent():
+            return self if predicate(self._item) else Nullable.empty()
+        return Nullable.empty()
+
+    def map(self, callable: Callable[[T], S]) -> "Nullable[S]":
+        """Maps the item when present.
+        Args:
+            callable (Callable): Invoked with a non-nil parameter.
+        """
+        if self.isPresent():
+            return Nullable(callable(self._item))
+        return Nullable.empty()
+
+    def __bool__(self) -> bool:
+        return self.isPresent()
+
+    @staticmethod
+    def empty() -> "Nullable":
+        return _empty
+
+
+_empty = Nullable(None)
+
+
+class EmptyNullableException(Exception):
+    pass
+
 
 class Stream(Generic[T]):
     """Stream class to perform functional-style operations in an aesthetically-pleasing manner.
@@ -52,17 +146,17 @@ class Stream(Generic[T]):
         """Returns True if no element of the stream matches the criteria."""
         return not self.anyMatch(fun)
 
-    def firstMatch(self, fun: Callable[[T], bool]) -> Optional[T]:
-        """Returns the first element matching the criteria. If none exist, returns None."""
+    def firstMatch(self, fun: Callable[[T], bool]) -> Nullable[T]:
+        """Returns a Nullable of the first element matching the criteria. If none exist, returns an empty Nullable."""
         if self.__should_expand(fun):
             for i in self:
                 if fun(*i):
-                    return i
+                    return Nullable(i)
         else:
             for i in self:
                 if fun(i):
-                    return i
-        return None
+                    return Nullable(i)
+        return Nullable.empty()
 
     def flat(self) -> "Stream[T]":
         """When iterating over lists, flattens the stream by concatenating all lists."""
@@ -165,6 +259,6 @@ class Stream(Generic[T]):
         else:
             return Stream(range(*args))
 
-    def first(self) -> Optional[T]:
-        """Returns the first element of the stream. If the stream is empty, returns None."""
-        return next(self.iterable, None)
+    def first(self) -> Nullable[T]:
+        """Returns a nullable containing the first element of the stream. If the stream is empty, returns an empty nullable."""
+        return Nullable(next(self.iterable, None))
