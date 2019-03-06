61879175ab79da8b0d2a757c28a51c41091af345
Kento A. Lauzon <kento.lauzon@ligature.ca>
2019-03-06 14:31:08 -0500

    fix docs formatting

--- jivago_streams/__init__.py
+++ jivago_streams/__init__.py
@@ -12,11 +12,11 @@ class Nullable(Generic[T]):
     """Nullable class which wraps Optional types.
 
     Args:
-        nullable (Optional[object)) : Item which can be None.
+        nullable (Optional) : Item which can be None.
     """
 
     def __init__(self, nullable: Optional[T]):
-        self._item: Optional[T] = nullable
+        self._item = nullable
 
     def isPresent(self) -> bool:
         """Returns True if item is not None."""
@@ -26,7 +26,7 @@ class Nullable(Generic[T]):
         """Gets the item if present.
 
         Raises:
-            EmptyNullableException: Attempting to get a missing item.
+            EmptyNullableException : Attempting to get a missing item.
             """
         if self.isPresent():
             return self._item
@@ -34,15 +34,17 @@ class Nullable(Generic[T]):
 
     def orElse(self, default_value: T) -> T:
         """Returns the item if present, else return the supplied default value.
+
         Args:
-            default_value: Value to return instead of a None value.
+            default_value : Value to return instead of a None value.
             """
         return self._item if self.isPresent() else default_value
 
     def orElseThrow(self, exception: Union[Exception, Callable[[], Exception]]) -> T:
         """Returns if present, raises exception if missing.
+
         Args:
-            exception: Either an exception, or a callable which returns an exception.
+            exception : Either an exception, or a callable which returns an exception.
         """
         if self.isPresent():
             return self._item
@@ -52,8 +54,9 @@ class Nullable(Generic[T]):
 
     def orElseFetch(self, supplier: Callable[[], T]) -> T:
         """Returns if present, invoke callable if missing.
+
         Args:
-            supplier (Callable): Supplied return value will be return in place of a None value. Should not require parameters.
+            supplier (Callable) : Supplied return value will be return in place of a None value. Should not require parameters.
         """
         if self.isPresent():
             return self._item
@@ -61,16 +64,18 @@ class Nullable(Generic[T]):
 
     def ifPresent(self, consumer: Callable[[T], None]) -> None:
         """Invoke function if value is present; otherwise does nothing.
+
         Args:
-            consumer (Callable): Function to be invoked with a non-nil parameter.
+            consumer (Callable) : Function to be invoked with a non-nil parameter.
         """
         if self.isPresent():
             consumer(self._item)
 
     def filter(self, predicate: Callable[[T], bool]) -> "Nullable[T]":
         """Filters item given a criterion.
+
         Args:
-            predicate (Callable): Invoked with a non-nil parameter. Should return a boolean.
+            predicate (Callable) : Invoked with a non-nil parameter. Should return a boolean.
 
         """
         if self.isPresent():
@@ -79,8 +84,9 @@ class Nullable(Generic[T]):
 
     def map(self, callable: Callable[[T], S]) -> "Nullable[S]":
         """Maps the item when present.
+
         Args:
-            callable (Callable): Invoked with a non-nil parameter.
+            callable (Callable) : Invoked with a non-nil parameter.
         """
         if self.isPresent():
             return Nullable(callable(self._item))
